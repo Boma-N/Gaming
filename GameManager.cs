0ddd9f7e54eeac09527b4dcd4a30c9af62fcb83d
Boma-N <108407186+Boma-N@users.noreply.github.com>
2026-02-17 00:46:44 +0100

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -1,1 +1,23 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public bool isGameOver = false;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        Debug.Log("GAME OVER");
+    }
+}
 
