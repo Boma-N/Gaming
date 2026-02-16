dc0c9a2430f6acd3b5cba5c2d52df95c0d12560f
Boma-N <108407186+Boma-N@users.noreply.github.com>
2026-02-17 00:48:50 +0100

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = transform.right * InputManager.Horizontal +
+                       transform.forward * InputManager.Vertical;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
 
