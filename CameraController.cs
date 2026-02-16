9bdd1d003d5ea3a8a29a9755b1103672042e45d9
Boma-N <108407186+Boma-N@users.noreply.github.com>
2026-02-17 00:49:51 +0100

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -1,1 +1,17 @@
+using UnityEngine;
+
+public class CameraController: MonoBehaviour
+{
+    public float sensitivity = 100f;
+    float xRotation = 0f;
+
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
 
